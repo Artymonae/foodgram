95e9c75bcc0a924709006401740e4fe3ec58a395
artymon <artymonevans@yandex.ru>
2025-04-03 02:11:26 +0400

    fix constants

--- backend/foodgram/users/constants.py
+++ backend/foodgram/users/constants.py
@@ -5,7 +5,6 @@ MAX_EMAIL_LENGTH = 255  # Максимальная длина почты.
 MAX_PASSWORD_LENGTH = 255  # Максимальная длина пароля.
 MAX_USERNAME_LENGTH = 150  # Максиальная длина никнейма.
 MAX_LENGTH_VALIDATION_FIELD = 150  # Максимальная длина валидации.
-LENGTH_SHORT_LINK = 12  # Длина короткой ссылки.
 
 USERNAME_REGEX = r"^[\w.@+-]+\Z"  # Регулярные выражения.
 NOT_ALLOWED_USERNAME = [
